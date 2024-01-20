3e2a4f8ff98de7c5e572fde21faadc801c1b1e5a
tuwafula <wafulaat@gmail.com>
2024-01-20 19:42:15 +0300

    finished book issue feature

--- src/pages/Transactions.js
+++ src/pages/Transactions.js
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import TransactionsTable from "../features/transactions/TransactionsTable";
+import Button from "../ui/Button";
 import Heading from "../ui/Heading";
+import Modal from "../ui/Modal";
 import Row from "../ui/Row";
+import IssueBookForm from "../features/transactions/IssueBookForm";
 
 function Transactions() {
+  const [showModal, setShowModal] = useState(false);
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All Transactions</Heading>
+        <Button onClick={() => setShowModal((state) => !state)}>
+          Issue Book
+        </Button>
+        {showModal && (
+          <Modal onClose={() => setShowModal((state) => !state)}>
+            <IssueBookForm
+              onCloseModal={() => setShowModal((state) => !state)}
+            />
+          </Modal>
+        )}
       </Row>
       <Row>
         <TransactionsTable />
